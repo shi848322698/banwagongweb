fb05a892be353b2c55e2e5fb20fb7888dd75d476
shi848322698 <848322698@qq.com>
2019-09-05 13:17:52 +0800

    增加了一个文章类的管理在后台

--- article/admin.py
+++ article/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from .models import ArticleColumn
+from .models import ArticleColumn, ArticlePost
 
 
 class ArticleColumnAdmin(admin.ModelAdmin):
     list_display = ('column', 'created', 'user')
     list_filter = ("column",)
 
+class ArticlePOSTAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'created')
+    list_filter = ("author",)
 
 admin.site.register(ArticleColumn, ArticleColumnAdmin)
+admin.site.register(ArticlePost, ArticlePOSTAdmin)
+
 
 # Register your models here.
